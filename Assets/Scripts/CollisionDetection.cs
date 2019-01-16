57264d5f101bd5e2550e239bc6b6e7fe9f8ed32f
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-01-16 11:41:52 +0100

    Added collision detection

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene("EndScene", LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
