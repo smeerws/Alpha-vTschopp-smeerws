c4eaf91fc3d765cb26694ebf8ca8db494286b64d
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-01-16 14:23:18 +0100

    Test script for random

---
+++ Assets/Scripts/RandomNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RandomNumbers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        RandomNumberGenerator1();
+        Debug.Log(RandomNumberGenerator2());
+
+        RandomNumberGenerator3(123, 1, 5);
+        Debug.Log(RandomNumberGenerator4(123, 1, 5));
+    }
+
+    public void RandomNumberGenerator1()
+    {
+        System.Random randomNumber = new System.Random(123);
+        Debug.Log(randomNumber.Next(1, 5));
+    }
+
+    public int RandomNumberGenerator2()
+    {
+        System.Random randomNumber = new System.Random(123);
+        return randomNumber.Next(1, 5);
+    }
+
+    public void RandomNumberGenerator3(int seed, int min, int max)
+    {
+        System.Random randomNumber = new System.Random(seed);
+        Debug.Log(randomNumber.Next(min, max));
+    }
+
+    public int RandomNumberGenerator4(int seed, int min, int max)
+    {
+        System.Random randomNumber = new System.Random(seed);
+        return randomNumber.Next(min, max);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
