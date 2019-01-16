089b64d02fc66cb004dea55b32292cbc5b6cfc21
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-01-16 01:00:10 +0100

    Added timer without functionality (yet)

---
+++ Assets/Scripts/StopWatch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StopWatch : MonoBehaviour {
+
+    private Text watch;
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        watch = gameObject.GetComponent<Text>();
+        watch.text = "00:00:00";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer = Time.deltaTime;
+	}
+}
