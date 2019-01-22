e6189c46c7160b6dd939b35bb88b8b2314bf5c15
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-01-22 10:47:18 +0100

    Added highscore functionality (WIP)

--- Assets/Scripts/Highscore.cs
+++ Assets/Scripts/Highscore.cs
@@ -17,9 +17,26 @@ public class Highscore : MonoBehaviour {
     {
         string path = "Assets/TextFiles/highscore.txt";
 
-        StreamWriter writer = new StreamWriter(path, true);
-        writer.WriteLine(watch.text);
-        writer.Close();
+        StreamReader reader = new StreamReader(path, true);
+        string[] highscore = reader.ReadToEnd().Split("\n"[0]);
+        string lastLine = highscore[highscore.Length - 2]; // Last line is empty, index starts at 0
+        reader.Close();
+
+        int lastLineInt = ConvertTimeToInt(lastLine);
+        int watchInt = ConvertTimeToInt(watch.text);
+
+        if(lastLineInt != watchInt)
+        {
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.WriteLine(watch.text);
+            writer.Close();
+        }
+    }
+
+    public int ConvertTimeToInt(string time)
+    {
+        string[] splittedString = time.Split("\n"[0]);
+        return result;
     }
 
     // Update is called once per frame
