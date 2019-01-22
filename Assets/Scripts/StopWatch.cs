70edc0b2a93b19f5128c7613e6694bb3518566cf
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-01-22 08:07:58 +0100

    Added watch functionality

--- Assets/Scripts/StopWatch.cs
+++ Assets/Scripts/StopWatch.cs
@@ -17,5 +17,10 @@ public class StopWatch : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         timer += Time.deltaTime;
-	}
+
+        watch.text = string.Format("{0:00}:{1:00}.{2:00}",
+            Mathf.Floor(timer / 60),
+            Mathf.Floor(timer) % 60,
+            Mathf.Floor((timer * 100) % 100));
+    }
 }
