bb14becf80b09227a52996a3887d27654daeb0e8
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-01-22 09:40:14 +0100

    Added highscore functionality

---
+++ Assets/Scripts/Highscore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class Highscore : MonoBehaviour {
+
+    private Text watch;
+
+	// Use this for initialization
+	void Start () {
+        watch = gameObject.GetComponent<Text>();
+	}
+
+    public void WriteHighscore()
+    {
+        string path = "Assets/TextFiles/highscore.txt";
+
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(watch.text);
+        writer.Close();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
